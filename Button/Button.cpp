02fe842a1044cdd354c2e31c11d63b3851dfb28c
hamapoly4 <168153463+hamapoly4@users.noreply.github.com>
2025-02-15 19:15:54 +0900

    Add files via upload

---
+++ Button/Button.cpp
@@ -0,0 +1,33 @@
+#include "Button.hpp"
+
+extern "C" {
+#include "spike/hub/button.h"
+}
+
+bool Button::isLeftPressed()
+{
+    hub_button_t pressed;
+    hub_button_is_pressed(&pressed);
+    return static_cast<bool>(pressed & HUB_BUTTON_LEFT);
+}
+
+bool Button::isCenterPressed()
+{
+    hub_button_t pressed;
+    hub_button_is_pressed(&pressed);
+    return static_cast<bool>(pressed & HUB_BUTTON_CENTER);
+}
+
+bool Button::isRightPressed()
+{
+    hub_button_t pressed;
+    hub_button_is_pressed(&pressed);
+    return static_cast<bool>(pressed & HUB_BUTTON_RIGHT);
+}
+
+bool Button::isBluetoothPressed()
+{
+    hub_button_t pressed;
+    hub_button_is_pressed(&pressed);
+    return static_cast<bool>(pressed & HUB_BUTTON_BT);
+}
---
+++ Button/Button.hpp
@@ -0,0 +1,12 @@
+#ifndef ___BUTTON_CLASS
+#define ___BUTTON_CLASS
+
+class Button {
+public:
+    bool isLeftPressed();
+    bool isCenterPressed();
+    bool isRightPressed();
+    bool isBluetoothPressed();
+};
+
+#endif // ___BUTTON_CLASS
