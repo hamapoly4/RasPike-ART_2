466e5f3721b0fa71ced1ca61ca26ce1c18d09471
hamapoly4 <168153463+hamapoly4@users.noreply.github.com>
2025-03-05 15:12:42 +0900

    Add files via upload

---
+++ 08_DebriRemove/enum_class.hpp
@@ -0,0 +1,14 @@
+#ifndef ___ENUM__CLASS
+#define ___ENUM__CLASS
+
+namespace debrisys {
+
+enum class ERotDire { LEFT, RIGHT, };
+
+enum class EStraDire { FRONT, BEHIND, };
+
+enum class EColor { RED, BLUE, };
+
+}   // namespace debrisys
+
+#endif // ___ENUM__CLASS
